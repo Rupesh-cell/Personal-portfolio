c094446ee04bc704e9b8172c8bbf84099351ad3a
Rupesh Khadka <78486743+Rupesh-cell@users.noreply.github.com>
2023-08-26 17:58:38 +0545

    idk

--- Frontend/src/Components/Techs.jsx
+++ Frontend/src/Components/Techs.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
-import '../Scss/techs.scss';
-import { Container } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import "../Scss/techs.scss";
+import { Container } from "react-bootstrap";
+import axios from "axios";
+
 
 const TechnologyProgressBar = () => {
+  const [tech, setTech] = useState([]);
+
+  const options = {
+    method: "GET",
+    url: "http://localhost:5000/api/techs",
+    headers: { "Content-Type": "application/json" },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      setTech(response.data)
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+    useEffect(() => {
+    
+    
+    }, [])
+    
+
+
   return (
     <Container>
-    <div className="technology-progress">
-      <div className="technology-logo">
-        <img src={logoUrl} alt={`${technology} Logo`} />
+      <div className="technology-progress">
+        <div className="technology-logo">
+          <img src={} alt={`${} Logo`} />
+        </div>
+        <div className="progress-bar">
+          <div className="progress" style={{ width: `${progress}%` }} />
+        </div>
       </div>
-      <div className="progress-bar">
-        <div className="progress" style={{ width: `${progress}%` }} />
-      </div>
-    </div>
     </Container>
   );
 };
 
 const FrontendPortfolio = () => {
   const technologies = [
-    { name: 'HTML', progress: 90, logoUrl: 'html-logo.png' },
-    { name: 'CSS', progress: 85, logoUrl: 'css-logo.png' },
-    { name: 'JavaScript', progress: 80, logoUrl: 'javascript-logo.png' },
+    { name: "HTML", progress: 90, logoUrl: "html-logo.png" },
+    { name: "CSS", progress: 85, logoUrl: "css-logo.png" },
+    { name: "JavaScript", progress: 80, logoUrl: "javascript-logo.png" },
     // Add more technologies and progress values here
   ];
 
